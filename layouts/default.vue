fa839b05831a87c3eee67be159f9d1b45716fd9c
wtrinaldi <williamtrinaldi@gmail.com>
2021-01-22 12:35:50 -0500

    fix errors

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,8 @@
   <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+      mini-variant
+      expand-on-hover
       fixed
       app
     >
@@ -24,22 +24,13 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-icon class="mx-2">mdi-grass</v-icon>
-      <v-toolbar-title v-text="title" />
+
+      <v-toolbar-title id="title" v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
+      <v-btn>
+        <v-icon>mdi-account-circle-outline</v-icon>
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -47,9 +38,9 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+    <v-navigation-drawer temporary fixed>
       <v-list>
-        <v-list-item @click.native="right = !right">
+        <v-list-item>
           <v-list-item-action>
             <v-icon light> mdi-repeat </v-icon>
           </v-list-item-action>
@@ -57,7 +48,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
+    <v-footer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -67,9 +58,7 @@
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
+      drawer: true,
       items: [
         {
           icon: 'mdi-apps',
@@ -80,13 +69,24 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire',
-        },
+        }, 
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Grasspedia',
     }
   },
 }
 </script>
+
+<style scoped>
+@font-face {
+  font-family: "Grass";
+  src: url(../static/Grassevent.ttf);
+}
+
+#title {
+  font-family: "Grass";
+  font-size: 3rem;
+
+}
+
+</style>
